5c90547ee1631f61511827c4041a4c0a1a2b9c1c
Logan Grisham <logangrisham@yahoo.com>
2021-09-07 21:58:43 -0500

    Read me and Index.js

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 const validatePassword = require('./passwordvalidation'); //import the function
 
 // Your code goes here!
+var readL = require('readL');
+var post = readL.createInterface(process.stdin, process.stdout);
+
+getPassword();
+
+function getPassword() {
+    post.question('Enter a password to validate (or :q to quit)', (password) => {
+
+        if (validatePassword(password) == true) {
+            Console.log("This is a valid password!");
+            return getPassword();
+
+        }
+        else if (validatePassword(password) == false) {
+            Console.log("This password is invalid, Please try again.")
+            return getPassword();
+        }
+        else if (validatePassword(password) == ':q') {
+            post.close;
+        }
+    });
+
+}
+
